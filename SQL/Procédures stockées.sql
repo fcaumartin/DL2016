ce72919604a3ef68db22e354e15c0f5de08b5d47
frc <fcaumartin@gmail.com>
2017-01-27 11:28:56 +0100

    proc vue

---
+++ SQL/Procédures stockées.sql
@@ -0,0 +1,9 @@
+create proc delete_all_produit
+as
+delete from produit
+
+create proc sup_client
+as
+
+
+exec tutu
---
+++ SQL/vues.sql
@@ -0,0 +1,28 @@
+create view toto
+as
+select distinct p.libart, f.nomfou
+from Fournisseur as f
+join vente as v on v.numfou=f.numfou
+join Produit as p on p.codart=v.codart
+where p.stkphy < p.stkale*1.5 and v.delliv<=30
+
+
+select * from toto where Produit.stkphy>100
+go
+drop view commandes
+go
+create view commandes
+as
+select f.*, p.*, e.datcom, e.obscom, l.derliv, l.numlig, l.priuni, l.qtecde, l.qteliv
+from Fournisseur as f
+join entcom as e on e.numfou=f.numfou
+join ligcom l on l.numcom=e.numcom
+join Produit as p on p.codart=l.codart
+
+
+
+create view titi
+as
+select * 
+from  commandes
+where numfou=0120
