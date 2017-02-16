493653e31fec09a67016a0e7e5abc871e3743c08
frc <fcaumartin@gmail.com>
2017-02-16 08:38:25 +0100

    Tok

---
+++ Boucles/Tok/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mot = "18/07/1982";
+            string sep = "/";
+            int compteur = 0;
+            int morceau = 2;
+            string resultat = "";
+
+
+            for (int i = 0; i < mot.Length; i++)
+            {
+                if (mot.Substring(i, 1) == sep)
+                {
+                    compteur++;
+                }
+                else {
+                    if (compteur == morceau)
+                        resultat += mot.Substring(i, 1);
+                }
+            }
+
+            Console.WriteLine(resultat);
+
+        }
+    }
+}
